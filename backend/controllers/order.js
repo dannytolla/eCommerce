933fb1b65045cb7af7e9c7538ba1dbb0d911b5e0
danny <danieltdanny99@gmial.com>
2021-02-22 17:21:14 +0300

    product and order added

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,80 +1,77 @@
-// const asyncHandler = require("express-async-handler");
-// const order = require("../models/Order");
+const asyncHandler = require("express-async-handler");
+const order = require("../models/Order");
+const ErrorResponse = require("../utils/errorResponse");
 
-// exports.addOrderItems = asyncHandler(async (req, res) => {
-//   const { orderItems } = req.body;
+exports.addOrderItems = asyncHandler(async (req, res, next) => {
+  const { orderItems } = req.body;
 
-//   if (orderItems && orderItems.length === 0) {
-//     res.status(404);
-//     throw new Error("No order items");
-//     return;
-//   } else {
-//     req.body.user = req.user.id;
+  if (orderItems && orderItems.length === 0) {
+    return next(new ErrorResponse("No order items", 404));
+    return;
+  } else {
+    req.body.user = req.user.id;
 
-//     const order = await Order.create(req.body);
+    const order = await Order.create(req.body);
 
-//     res.status(201).json(order);
-//   }
-// });
+    res.status(201).json(order);
+  }
+});
 
-// exports.getOrderById = asyncHandler(async (req, res) => {
-//   const order = await Order.findById(req.params.id).populate({
-//     path: "User",
-//     select: "name email",
-//   });
+exports.getOrderById = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate({
+    path: "User",
+    select: "name email",
+  });
 
-//   if (order) {
-//     res.json(order);
-//   } else {
-//     res.status(404);
-//     throw new Error("Order not found");
-//   }
-// });
+  if (order) {
+    res.json(order);
+  } else {
+    return next(new ErrorResponse("Order not found", 404));
+  }
+});
 
-// exports.updateOrderToPaid = asyncHandler(async (req, res) => {
-//   const order = await Order.findById(req.params.id);
+exports.updateOrderToPaid = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
 
-//   if (order) {
-//     order.paid = true;
-//     order.paidAt = Date.now();
-//     order.paymentResult = {
-//       id: req.body.id,
-//       status: req.body.status,
-//       update_time: req.body.update_time,
-//       email_address: req.body.payer.email_address,
-//     };
+  if (order) {
+    order.paid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
 
-//     const updateOrder = await order.save();
+    const updateOrder = await order.save();
 
-//     res.json(updateOrder);
-//   } else {
-//     res.status(404);
-//     throw new Error("Order not found");
-//   }
-// });
+    res.json(updateOrder);
+  } else {
+    return next(new ErrorResponse("Order not found", 404));
+  }
+});
 
-// exports.updateOrderToDelivered = asyncHandler(async (req, res) => {
-//   const order = await Order.findById(req.params.id);
+exports.updateOrderToDelivered = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
 
-//   if (order) {
-//     order.isDelivered = true;
-//     order.deliveredAt = Date.now();
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
 
-//     const updatedOrder = await order.save();
+    const updatedOrder = await order.save();
 
-//     res.json(updatedOrder);
-//   } else {
-//     res.status(404);
-//     throw new Error("Order not found");
-//   }
-// });
+    res.json(updatedOrder);
+  } else {
+    return next(new ErrorResponse("Order not found", 404));
+  }
+});
 
-// exports.getMyOrders = asyncHandler(async (req, res) => {
-//   const orders = await Order.find({ user: req.user.id });
-//   res.json(orders);
-// });
+exports.getMyOrders = asyncHandler(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id });
+  res.json(orders);
+});
 
-// exports.getOrders = asyncHandler(async (req, res) => {
-//   const orders = await Order.find({}).populate("user", "id name");
-//   res.json(orders);
-// });
+exports.getOrders = asyncHandler(async (req, res, next) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,128 +1,124 @@
-// const asyncHandler = require("express-async-handler");
-// const Product = require("../models/Product");
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/Product");
+const ErrorResponse = require("../utils/errorResponse");
 
-// exports.getProducts = asyncHandler(async (req, res) => {
-//   const pageSize = 10;
-//   const page = Number(req.query.pageNumber) || 1;
+exports.getProducts = asyncHandler(async (req, res, next) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
 
-//   const keyword = req.query.keyword
-//     ? {
-//         name: {
-//           $regex: req.query.keyword,
-//           $options: "i",
-//         },
-//       }
-//     : {};
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
 
-//   const count = await Product.countDocument({ ...keyword });
-//   const products = await Product.find({ ...keyword })
-//     .limit(pageSize)
-//     .skip(pageSize * (page - 1));
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
 
-//   res.json({ products, page, pages: Math.ceil(count / pageSize) });
-// });
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
+});
 
-// exports.getProductById = asyncHandler(async (req, res) => {
-//   const product = await Product.findById(req.params.id);
+exports.getProductById = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-//   if (product) {
-//     res.json(product);
-//   } else {
-//     res.status(404);
-//     throw new Error("Product not found");
-//   }
-// });
+  if (product) {
+    res.json({ product });
+  } else {
+    return next(new ErrorResponse("Product not found", 404));
+  }
+});
 
-// exports.deleteProduct = asyncHandler(async (req, res) => {
-//   const product = await Product.findById(req.params.id);
+exports.deleteProduct = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-//   if (product) {
-//     await product.remove();
-//     res.json({ msg: "Product Removed" });
-//   } else {
-//     res.status(404);
-//     throw new Error("Product not found");
-//   }
-// });
+  if (product) {
+    await product.remove();
+    res.json({ msg: "Product Removed" });
+  } else {
+    return next(new ErrorResponse("Product not found", 404));
+  }
+});
 
-// exports.createProduct = asyncHandler(async (req, res) => {
-//   req.body.user = req.user.id;
-//   const product = await Product.create(req.body);
+exports.createProduct = asyncHandler(async (req, res, next) => {
+  req.body.user = req.user.id;
+  const product = await Product.create(req.body);
 
-//   res.status(201).json(product);
-// });
+  res.status(201).json({ product });
+});
 
-// exports.updateProduct = asyncHandler(async (req, res) => {
-//   const {
-//     name,
-//     price,
-//     description,
-//     image,
-//     brand,
-//     category,
-//     countInStock,
-//   } = req.body;
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
 
-//   const fieldToUpdate = {
-//     name,
-//     price,
-//     description,
-//     image,
-//     brand,
-//     category,
-//     countInStock,
-//   };
+  const fieldToUpdate = {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  };
 
-//   const product = await Product.findOneAndUpdate(req.params.id, fieldToUpdate, {
-//     new: true,
-//     runValidators: true,
-//   });
+  const product = await Product.findOneAndUpdate(req.params.id, fieldToUpdate, {
+    new: true,
+    runValidators: true,
+  });
 
-//   if (!product) {
-//     res.status(404);
-//     throw new Error("Product not found");
-//   }
-// });
+  if (!product) {
+    return next(new ErrorResponse("Product not found", 404));
+  }
+});
 
-// exports.createProductReview = asyncHandler(async (req, res) => {
-//   const { rating, comment } = req.body;
+exports.createProductReview = asyncHandler(async (req, res, next) => {
+  const { rating, comment } = req.body;
 
-//   const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
 
-//   if (product) {
-//     const alreadyReviewed = product.reviews.find(
-//       (r) => r.user.toString() === req.user.id.toString()
-//     );
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user.id.toString()
+    );
 
-//     if (alreadyReviewed) {
-//       res.status(400);
-//       throw new Error("Product alread reviewed");
-//     }
+    if (alreadyReviewed) {
+      return next(new ErrorResponse("Product already reviewed", 400));
+    }
 
-//     const review = {
-//       name: req.user.name,
-//       rating: rating(Number),
-//       comment,
-//       user: req.user.id,
-//     };
+    const review = {
+      name: req.user.name,
+      rating: rating(Number),
+      comment,
+      user: req.user.id,
+    };
 
-//     product.review.push(review);
-//     product.numReviews = product.reviews.length;
+    product.review.push(review);
+    product.numReviews = product.reviews.length;
 
-//     product.rating =
-//       product.reviews.reduce((acc, item) => item.rating + acc, 0) /
-//       product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
 
-//     await product.save();
-//     res.status(201).json({ msg: "Review added" });
-//   } else {
-//     res.status(404);
-//     throw new Error("Product not found");
-//   }
-// });
+    await product.save();
+    res.status(201).json({ msg: "Review added" });
+  } else {
+    return next(new ErrorResponse("Product not found", 404));
+  }
+});
 
-// exports.getTopProducts = asyncHandler(async (req, res) => {
-//   const products = await Products.find({}).sort({ rating: -1 }).limit(3);
+exports.getTopProducts = asyncHandler(async (req, res, next) => {
+  const products = await Products.find({}).sort({ rating: -1 }).limit(3);
 
-//   res.json(products);
-// });
+  res.json(products);
+});
