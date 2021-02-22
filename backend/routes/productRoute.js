8c8693891b557df2fa4ed9afc84fb4c8731d94ed
danny <danieltdanny99@gmial.com>
2021-02-22 17:23:10 +0300

    product route added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,25 +1,25 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// const { protect, admin } = require("../middleware/auth");
+const { protect, admin } = require("../middleware/auth");
 
-// const {
-//   getProducts,
-//   getProductById,
-//   createProduct,
-//   deleteProduct,
-//   updateProduct,
-//   createProductReview,
-//   getTopProducts,
-// } = require("../controllers/product");
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} = require("../controllers/product");
 
-// router.route("/").get(getProducts).post(protect, admin, createProduct);
-// router.route("/:id/review").post(protect, createProductReview);
-// router.route("/top").post(getTopProducts);
-// router
-//   .route("/:id")
-//   .get(getProductById)
-//   .put(protect, admin, updateProduct)
-//   .delete(protect, admin, deleteProduct);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/review").post(protect, createProductReview);
+router.route("/top").post(getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
-// module.exports = router;
+module.exports = router;
